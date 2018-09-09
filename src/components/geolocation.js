edf345d0b6f508a8105f2b0256da527559ab41c0
Patricia Ciobanu <ciobanu.paty@gmail.com>
2018-09-09 04:03:03 +0200

    Get sequence layer

--- src/components/geolocation.js
+++ src/components/geolocation.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import {geolocated} from 'react-geolocated';
+import { modelInstance } from '../model/model';
 
 class Geolocation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          coordinates: this.props.coords,
+          body: null,
+          user_id: null,
+          sound_id: null,
+          status: 'INITIAL'
+        }
+    }
+
+    getData = () => {
+        modelInstance.fetch_geolocation(this.props.coords)
+        .then(response => {
+          this.setState({ 
+              user_id: response[0].user_id,
+              sound_id: response[0].sound_id, 
+            });
+        });
+    }
+
     render() {
       return !this.props.isGeolocationAvailable
         ? <div>Your browser does not support Geolocation</div>
@@ -9,11 +31,14 @@ class Geolocation extends React.Component {
           ? <div>Geolocation is not enabled</div>
           : this.props.coords
             ? <div>
+                {this.getData()}
                 <p>latitude: {this.props.coords.latitude}</p>
                 <p>longitude: {this.props.coords.longitude}</p>
                 <p>altitude: {this.props.coords.altitude}</p>
                 <p>heading: {this.props.coords.heading}</p>
                 <p>speed: {this.props.coords.speed}</p>
+                <p>user_id: {this.state.user_id}</p>
+                <p>sound_id: {this.state.sound_id}</p>
             </div>
             : <div>Getting the location data&hellip; </div>;
     }
--- src/model/model.js
+++ src/model/model.js
@@ -45,6 +45,35 @@ const Model = function () {
     this.getSounds = function(){
         return sounds;
     };
+
+    this.fetch_geolocation = function(params){
+        const url = "http://130.229.135.119:5002/sequence?location=" + params.latitude + ';' + params.longitude;
+        return fetch(url)
+        .then(processResponse)
+        .catch(handleError)
+    }
+
+    // this.fetch_userId = function(){
+    //     let userId_json = generic_fetch("user_id");
+    //     return userId_json.user_id;   
+    // }
+
+    const processResponse = function (response) {
+        if (response.ok) {
+          return response.json()
+        }
+        throw response;
+    }
+     
+    const handleError = function (error) {
+        if (error.json) {
+          error.json().then(error => {
+            console.error('API Error:', error.message || error)
+          })
+        } else {
+          console.error('API Error:', error.message || error)
+        }
+    }
 };
 
 export const modelInstance = new Model();
