c43f212605daa4da8bfd82b99a2b9728cfc0d46d
Quentin LEMAIRE <lemairequentin22@gmail.com>
2018-09-09 02:25:19 +0200

    unable high accuracy

--- src/components/geolocation.js
+++ src/components/geolocation.js
@@ -18,10 +18,10 @@ class Geolocation extends React.Component {
             : <div>Getting the location data&hellip; </div>;
     }
   }
-   
+
   export default geolocated({
     positionOptions: {
-      enableHighAccuracy: false,
+      enableHighAccuracy: true,
     },
     userDecisionTimeout: 5000,
   })(Geolocation);
